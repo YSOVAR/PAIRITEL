460f69ae59c5f40493be98fd5d3fee880cb19ef2
kpoppenhaeger <kpoppenhaeger@cfa.harvard.edu>
2013-05-28 17:29:59 -0400

    Changed astropy_4 so that only non-YSO surces are used for cross-calibration with 2MASS; also, errors on magnitudes are checked against reduced chisquared. Daophot underestimates errors quite a bit, and corrected errors are now calulated.

--- pairitel_astropy_4.py
+++ pairitel_astropy_4.py
@@ -10,6 +10,9 @@ import glob
 import os
 from scipy import optimize
 from astropy.table import Table, Column
+import sys
+sys.path.append('/data/swolk/kpoppen/Dropbox/MyPython/YSOVAR/YSOVAR-master')
+from great_circle_dist import dist_radec, dist_radec_fast
 
 import urllib
 import StringIO
@@ -22,36 +25,28 @@ import photometry_both
 reload(photometry_both)
 
 
-# load 2mass catalogue from disk:
-data2mass = asciitable.read(input_info.resultfolder + '2mass_for_cal.dat')
-
-# find all .als.1 files:
-filepath =  input_info.resultfolder + '*YSO*/*.als.1' 
-filelist = glob.glob(filepath)
-filelist.sort()
-
-f = plt.figure()
-chisq = np.ones(len(filelist))*-99999.
-chisq_real = np.ones(len(filelist))*-99999.
-
 
 # now transform found pairitel sources into wcs, find matching 2mass sources, and calibrate pairitel against 2mass. I use nominal errors of 0.1 mag for all pairitel sources instead of the output errors from the psf photometry. This is because sometimes there is large variability in a bright source (which has a small photometric error), which then would mess up the whole line fitting because that data point would have so much weight.
-for filename in filelist[:]:
+
+def check_band(filename):
+    checkband = os.path.basename(filename)[0]
+    (band2m, band2mp, band2mpe, band2mpc, band2mpce) = ('', '', '', '', '')
+    if checkband.lower() == 'h':
+	(band2m, band2mp, band2mpe, band2mpc, band2mpce) = ('2H', 'PH', 'PH_err', 'PH_cal', 'PH_cal_err')
+    else:
+	if checkband.lower() == 'k':
+	    (band2m, band2mp, band2mpe, band2mpc, band2mpce) = ('2K', 'PK', 'PK_err', 'PK_cal', 'PK_cal_err')
+	else:
+	    if checkband.lower() == 'j':
+		(band2m, band2mp, band2mpe, band2mpc, band2mpce) = ('2J', 'PJ', 'PJ_err', 'PJ_cal', 'PJ_cal_err')
     
+    return(checkband, band2m, band2mp, band2mpe, band2mpc, band2mpce)
+
+def match_pairitel_2mass_g12(filename, data2mass, g12filename, matchradius, writetofile=False):
     imagefile = filename[0:-6]
     
     pairitel = asciitable.read(filename, Reader=asciitable.Daophot)
-    checkband = os.path.basename(filename)[0]
-    if checkband == 'h':
-	(band2m, band2mp, band2mpe, band2mpc, band2mpce) = ('2H', 'PH', 'PH_err', 'PH_cal', 'PH_cal_err')
-    else:
-	if checkband == 'k':
-	    (band2m, band2mp, band2mpe, band2mpc, band2mpce) = ('2K', 'PK', 'PK_err', 'PK_cal', 'PK_cal_err')
-	else:
-	    if checkband == 'j':
-		(band2m, band2mp, band2mpe, band2mpc, band2mpce) = ('2J', 'PJ', 'PJ_err', 'PJ_cal', 'PJ_cal_err')
-    
-    print checkband
+    (checkband, band2m, band2mp, band2mpe, band2mpc, band2mpce) = check_band(filename)
     
     pairitel.add_column(Column(data = np.ones(len(pairitel['ID']))*-99999., name='RA'))
     pairitel.add_column(Column(data = np.ones(len(pairitel['ID']))*-99999., name='DEC'))
@@ -60,6 +55,7 @@ for filename in filelist[:]:
     pairitel.add_column(Column(data = np.ones(len(pairitel['ID']))*-99999., name=band2m))
     pairitel.add_column(Column(data = np.ones(len(pairitel['ID']))*-99999., name=band2mpc))
     pairitel.add_column(Column(data = np.ones(len(pairitel['ID']))*-99999., name=band2mpce))
+    pairitel.add_column(Column(data = np.ones(len(pairitel['ID']))*-99999., name='ysoflag'))
     
     pairitel[band2mp] = pairitel['MAG']
     pairitel[band2mpe] = pairitel['MERR']
@@ -78,23 +74,51 @@ for filename in filelist[:]:
     
     
     # find best-matching 2mass source for each pairitel source; if distance small enough, add the 2mass magnitude to the pairitel table.
+    # and check which 2mass-pairitel sources are not YSOs.
+    g12 = asciitable.read(g12filename)
+    g12 = g12._data.data
+    g12 = Table(g12)
     
-    matchradius = 1./3600.
     
     for i in np.arange(0, len(pairitel)):
-	dist = np.sqrt((data2mass['ra'] - pairitel[i]['RA'])**2 + (data2mass['dec'] - pairitel[i]['DEC'])**2)
-	mindist = dist.min()
-	if mindist < matchradius:
-	    ind = np.where(dist == mindist)[0]
+        # matching with 2mass:
+	dist = dist_radec_fast(pairitel[i]['RA'], pairitel[i]['DEC'], data2mass['ra'], data2mass['dec'], scale = matchradius, unit ='deg')
+	if dist.min() < matchradius:
+	    ind = np.where(dist == dist.min())[0]
 	    pairitel[i][band2m] = data2mass[ind][band2m]
+	
+	# matching with Guenther+2012 and flagging stuff that is a YSO:
+	#ind_near = np.where(np.abs(g12['DEdeg'] - pairitel[i]['DEC']) <= r_match)[0]
+	#if len(ind_near) > 0:
+	distg12 = dist_radec_fast(pairitel[i]['RA'], pairitel[i]['DEC'], g12['RAdeg'], g12['DEdeg'], scale = matchradius, unit ='deg')
+	ig12 = np.where(distg12 == distg12.min())[0]
+	if ((distg12.min() < matchradius) & ((g12[ig12]['Class'] == 'star') | (g12[ig12]['Class'] == ''))):
+	    pairitel[i]['ysoflag'] = 0.
     
     print np.where(pairitel[band2m]>-99999.)
     
+    if writetofile == True:
+        asciitable.write(pairitel, os.path.dirname(filename) + '/pairitel_matched_' + checkband + '.dat')
+    
+    return pairitel
+
+
+
+
+def fit_to_2mass(filename, pairitel, chisq, chisq_const, chisq_real, chisq_real_const, writetofile=False):
+    
+    (checkband, band2m, band2mp, band2mpe, band2mpc, band2mpce) = check_band(filename)
+    
+    pairitel = asciitable.read(os.path.dirname(filename)+'/pairitel_matched_'+os.path.basename(filename)[0]+'.dat')
     good = np.where(pairitel[band2m]>-99999.)[0]
+    good_const = np.where((pairitel[band2m]>-99999.) & (pairitel['ysoflag'] > -99999.))[0]
+    print good
+    print good_const
     
     f.clf()
     ax = f.add_subplot(111)
     ax.plot(pairitel[good][band2m], pairitel[good][band2mp], '.')
+    ax.plot(pairitel[good_const][band2m], pairitel[good_const][band2mp], 'r.')
     #plt.show()
     
     # fit straight line to that.
@@ -108,36 +132,158 @@ for filename in filelist[:]:
     pinit = [1.0, -1.0]
     out = optimize.leastsq(errfunc, pinit, args=(pairitel[good][band2m], pairitel[good][band2mp], error), full_output=1)
     pfinal = out[0]
+    pfinal_const = optimize.leastsq(errfunc, pinit, args=(pairitel[good_const][band2m], pairitel[good_const][band2mp], error[0:len(good_const)]), full_output=1)[0]
+    
     x = np.linspace(np.min(pairitel[good][band2m]), np.max(pairitel[good][band2m]), 100)
-    ax.plot(x, pfinal[0] + pfinal[1]*x)
+    ax.plot(x, pfinal[0] + pfinal[1]*x, 'b-')
+    ax.plot(x, pfinal_const[0] + pfinal_const[1]*x, 'r-')
     
-    # calculate new pairitel magnitudes.
-    pairitel[band2mpc] = (pairitel[band2mp] - pfinal[0])/pfinal[1]
-    pairitel[band2mpce] = (pairitel[band2mpe])/pfinal[1]
+    # calculate new pairitel magnitudes, using the 2mass fit to the non-YSO sources
+    pairitel[band2mpc] = (pairitel[band2mp] - pfinal_const[0])/pfinal_const[1]
+    pairitel[band2mpce] = (pairitel[band2mpe])/pfinal_const[1]
     
-    print pairitel[good][band2mpc]
-    print pairitel[good][band2mp]
-    print pairitel[good][band2m]
+    #print pairitel[good][band2mpc]
+    #print pairitel[good][band2mp]
+    #print pairitel[good][band2m]
     
+    # calculate several chi-squared values.
+    # chisq = all 2mass matches, using constant errors
     n = np.where(filename == np.array(filelist))[0][0]
-    #chisq[n] = np.sum(((pairitel[good][band2mp] - (pfinal[0] + pfinal[1]*pairitel[good][band2m]))/(pairitel[good][band2mpe]))**2.)/len(pairitel[good])
     chisq[n] = np.sum(((pairitel[good][band2mp] - (pfinal[0] + pfinal[1]*pairitel[good][band2m]))/(error))**2.)/len(pairitel[good])
+    # chisq_const = 2mass matches which are non-YSOs, using constant errors 
+    chisq_const[n] = np.sum(((pairitel[good_const][band2mp] - (pfinal[0] + pfinal[1]*pairitel[good_const][band2m]))/(error[0:len(good_const)]))**2.)/len(pairitel[good_const])
+    # chisq_real =  all 2mass matches, using daophot errors
     chisq_real[n] = np.sum(((pairitel[good][band2mp] - (pfinal[0] + pfinal[1]*pairitel[good][band2m]))/(pairitel[good][band2mpe]))**2.)/len(pairitel[good])
+    # chisq_real_const = 2mass matches which are non-YSOs, using daophot errors
+    chisq_real_const[n] = np.sum(((pairitel[good_const][band2mp] - (pfinal[0] + pfinal[1]*pairitel[good_const][band2m]))/(pairitel[good_const][band2mpe]))**2.)/len(pairitel[good_const])
     
-    ax.text(0.3, 0.7, 'red. chi**2 = ' + str(np.round(chisq[n],1)), horizontalalignment='center', verticalalignment='center', transform = ax.transAxes)
+    # collect the difference of the calibrated Pairitel magnitudes to the fit derived from the non-YSO sources.
+    pairitel.add_column(Column(data = np.ones(len(pairitel['ID']))*-99999., name='deltafit'))
+    for ind in good_const:
+        pairitel[ind]['deltafit'] = (pairitel[ind][band2mpc] - pairitel[ind][band2m])
+    
+    ax.text(0.3, 0.7, 'red. chi**2 = ' + str(np.round(chisq_real[n],1)), horizontalalignment='center', verticalalignment='center', transform = ax.transAxes)
+    ax.text(0.3, 0.8, 'red. chi**2, non-YSO = ' + str(np.round(chisq_real_const[n],1)), horizontalalignment='center', verticalalignment='center', transform = ax.transAxes)
     plt.show()
     
+    # correct errors so that reduced chisquared = 1 for non-YSO sources.
+    pairitel.add_column(Column(data = np.ones(len(pairitel['ID']))*-99999., name=band2mpce + '_corr'))
+    if chisq_real_const[n] > 1.:
+        pairitel[band2mpce + '_corr'] = pairitel[band2mpce] * np.sqrt(chisq_real_const[n])
+    else:
+        pairitel[band2mpce + '_corr'] = pairitel[band2mpce]
+    
     # write results to file.
-    asciitable.write(pairitel, os.path.dirname(filename) + '/calibratedmags_' + checkband + '.dat')
+    if writetofile == True:
+        asciitable.write(pairitel, os.path.dirname(filename) + '/calibratedmags_' + checkband + '.dat')
+    
+    return (chisq, chisq_const, chisq_real, chisq_real_const, pfinal, pfinal_const, pairitel)
 
-# if necessary, do some manual checking for high chisquared values.
-print np.where(chisq > 5)[0]
-plt.figure()
-plt.hist(chisq, bins=100)
-plt.title('Distribution of reduced chi**2 values (using constant errors of 0.1 mag)')
+def get_all_caldata():
+    filepath =  input_info.resultfolder + '*YSO*/calibratedmags_j.dat'
+    filelist = glob.glob(filepath)
+    filelist.sort()
+    
+    mydict = dict({})
+    
+    for b in ['j', 'h', 'k']:
+        filepath =  input_info.resultfolder + '*YSO*/calibratedmags_'+ b +'.dat'
+        filelist = glob.glob(filepath)
+        filelist.sort()
+            
+        data = asciitable.read(filelist[0])
+	for filename in filelist[:]:
+	    dat_tmp = asciitable.read(filename)
+	    for i in np.arange(0, len(dat_tmp)):
+		data.add_row(dat_tmp[i])
+        
+        mydict['data' + b.upper()] = data
+    
+    return mydict
 
-plt.figure()
-plt.hist(chisq_real, bins=100)
-plt.title('Distribution of reduced chi**2 values (using daophot errors)')
+
+def check_calmags(data, band):
+    (checkband, band2m, band2mp, band2mpe, band2mpc, band2mpce) = check_band(band)
+    good = np.where(data['deltafit']>-99999.)[0]
+    #plt.figure()
+    #plt.plot(data[good][band2mpc], data[good]['deltafit'], 'bo')
+    #plt.plot(data[good][band2mpc], data[good]['deltafit']/data[good][band2mpce]/50., 'ro')
+    
+    #good_bright = np.where((data['deltafit']>-99999.) & (data[band2mpc] < 13.))[0]
+    #good_dim = np.where((data['deltafit']>-99999.) & (data[band2mpc] >= 13.))[0]
+    
+    #plt.figure()
+    #plt.hist(data[good_bright]['deltafit'], bins=50, alpha=0.5)
+    #plt.hist(data[good_dim]['deltafit'], bins=50, alpha=0.5)
+    
+    #plt.figure()
+    #plt.hist(data[good_bright]['deltafit']/data[good_bright][band2mpce], bins=50, alpha=0.5)
+    #plt.hist(data[good_dim]['deltafit']/data[good_dim][band2mpce], bins=50, alpha=0.5)
+    
+    plt.clf()
+    errs=data[good]['deltafit']/data[good][band2mpce + '_corr']
+    plt.hist(errs, bins=50, alpha=0.5)
+    errs.sort()
+    i_low = int(0.16*len(errs))
+    i_high = int(0.84*len(errs))
+    #print errs[i_low], errs[i_high]
+    
+    errs_new=data[good]['deltafit']/data[good][band2mpce]/np.mean(np.array([np.abs(errs[i_low]), np.abs(errs[i_high])]))
+    #plt.figure()
+    #plt.hist(errs_new, bins=50, alpha=0.5)
+    errs_new.sort()
+    i_low = int(0.16*len(errs_new))
+    i_high = int(0.84*len(errs_new))
+    #print errs_new[i_low], errs_new[i_high]
+    
+    return np.mean(np.array([np.abs(errs[i_low]), np.abs(errs[i_high])]))
+
+
+
+# load 2mass catalogue from disk:
+data2mass = asciitable.read(input_info.resultfolder + '2mass_for_cal.dat')
+
+# find all .als.1 files:
+filepath =  input_info.resultfolder + '*YSO*/*.als.1' 
+filelist = glob.glob(filepath)
+filelist.sort()
+
+# find matching sources in 2mass and guenther+2012 for each source in each night.
+for filename in filelist[:]:
+    pairitel = match_pairitel_2mass_g12(filename, data2mass, g12filename='/swiper.real/kpoppen/IR/YSOVAR/iras20050/Guenther2012_table.txt', matchradius=1./3600., writetofile=True)
+
+
+# initialize output arrays
+chisq = np.ones(len(filelist))*-99999.
+chisq_const = np.ones(len(filelist))*-99999.
+chisq_real = np.ones(len(filelist))*-99999.
+chisq_real_const = np.ones(len(filelist))*-99999.
+
+# initialize plot window
+f = plt.figure()
+
+# fit pairitel data to 2mass data, using non-YSO sources as identified by guenther+2012
+for filename in filelist[:]:
+    (chisq, chisq_const, chisq_real, chisq_real_const, pfinal, pfinal_const, pairitel) = fit_to_2mass(filename, pairitel, chisq, chisq_const, chisq_real, chisq_real_const, writetofile=True)
+
+# collect all data from all night in a dictionary
+datadict = get_all_caldata()
+
+
+
+#corrfactor = np.zeros(3)
+#for b in np.array(['J', 'H', 'K']):
+    #corrfactor[np.where(b == np.array(['J', 'H', 'K']))[0]] = check_calmags(datadict['data'+b][:], b)
+
+#print datadict['dataJ']['PJ_cal_err_corr']/datadict['dataJ']['PJ_cal_err']
+
+#ratio = datadict['dataJ']['PJ_cal_err_corr']/datadict['dataJ']['PJ_cal_err']
+#good = np.where(np.isnan(ratio) == False) 
+#print ratio[good].min()
+#print ratio[good].max()
+
+#plt.hist(ratio[good], bins=100)
+
+
 
 
--- pairitel_astropy_5.py
+++ pairitel_astropy_5.py
@@ -10,6 +10,8 @@ import glob
 import os
 from scipy import optimize
 from astropy.table import Table, Column
+sys.path.append('/data/swolk/kpoppen/Dropbox/MyPython/YSOVAR/YSOVAR-master')
+from great_circle_dist import dist_radec, dist_radec_fast
 
 import urllib
 import StringIO
@@ -53,45 +55,84 @@ bands = ['H', 'K', 'J']
 (ra, dec) = photometry_wcs.coords_from_ds9(input_info.masterregfile)
 p_id = np.arange(0, len(ra))
 
+def make_lcs_from_disk():
+    for b in bands[:]:
+	a1=[]
+	a2=[]
+	a3=[]
+	a4=[]
+	a5=[]
+	a6=[]
+	a7=[]
+	a8=[]
+	a9=[]
+	a10=[]
+	names = ['Pair_ID', 'ra', 'dec', 't', 'H', 'H_err', 'J', 'J_err', 'K', 'K_err']
+	tab = Table([a1, a2, a3, a4, a5, a6, a7, a8, a9, a10], names=names)
+	
+	print b
+	filepath =  input_info.resultfolder + '*YSO*/calibratedmags_' + b.lower() + '.dat' 
+	filelist = glob.glob(filepath)
+	filelist.sort()
+	for i in p_id:
+	    print i
+	    for j in np.arange(0, len(filelist[:])):
+		# find source in calibratedmags file of a given night.
+		a = asciitable.read(filelist[j])
+		# look only at the ones which are near in dec:
+		ind_near = np.where(np.abs(a['DEC'] - dec[i]) <= r_match)[0]
+		# do full distance calculation for those:
+		if len(ind_near) > 0:
+		    distance = np.sqrt((ra[i] - a[ind_near]['RA'])**2 + (dec[i] - a[ind_near]['DEC'])**2)
+		    ind = np.where(distance == distance.min())[0][0]
+		    tab.add_row(np.ones(len(names))*np.nan)
+		    tab[-1]['Pair_ID'] = i
+		    tab[-1]['ra'] = ra[i]
+		    tab[-1]['dec'] = dec[i]
+		    tab[-1]['t'] = a[0]['t'] - 2400000.5 # all lines in a given nightly file have the same time stamp.
+		    tab[-1][b] = a[ind_near[ind]]['P' + b + '_cal']
+		    tab[-1][b + '_err'] = a[ind_near[ind]]['P' + b + '_cal_err_corr']
+	
+	asciitable.write(tab, input_info.resultfolder + b + '_pairitel.csv' )
 
-for b in bands[:]:
-    a1=[]
-    a2=[]
-    a3=[]
-    a4=[]
-    a5=[]
-    a6=[]
-    a7=[]
-    a8=[]
-    a9=[]
-    a10=[]
-    names = ['Pair_ID', 'ra', 'dec', 't', 'H', 'H_err', 'J', 'J_err', 'K', 'K_err']
-    tab = Table([a1, a2, a3, a4, a5, a6, a7, a8, a9, a10], names=names)
-        
-    print b
-    filepath =  input_info.resultfolder + '*YSO*/calibratedmags_' + b.lower() + '.dat' 
-    filelist = glob.glob(filepath)
-    filelist.sort()
-    for i in p_id:
-        print i
-	for j in np.arange(0, len(filelist[:])):
-	    # find source in calibratedmags file of a given night.
-	    a = asciitable.read(filelist[j])
-	    # look only at the ones which are near in dec:
-	    ind_near = np.where(np.abs(a['DEC'] - dec[i]) <= r_match)[0]
-	    # do full distance calculation for those:
-	    if len(ind_near) > 0:
-	        distance = np.sqrt((ra[i] - a[ind_near]['RA'])**2 + (dec[i] - a[ind_near]['DEC'])**2)
-		ind = np.where(distance == distance.min())[0][0]
-		tab.add_row(np.ones(len(names))*np.nan)
+
+def make_lcs():
+    for b in bands[:]:
+	a1=[]
+	a2=[]
+	a3=[]
+	a4=[]
+	a5=[]
+	a6=[]
+	a7=[]
+	a8=[]
+	a9=[]
+	a10=[]
+	names = ['Pair_ID', 'ra', 'dec', 't', 'H', 'H_err', 'J', 'J_err', 'K', 'K_err']
+	tab = Table([a1, a2, a3, a4, a5, a6, a7, a8, a9, a10], names=names)
+	print b
+	for i in p_id:
+	    print i
+	
+	    distance = dist_radec_fast(ra[i], dec[i], datadict['data' + b]['RA'], datadict['data' + b]['DEC'], scale = r_match, unit ='deg')
+	    ind = np.where(distance < r_match)[0]
+	    
+	    for i1 in ind:
+	        
+	        tab.add_row(np.ones(len(names))*np.nan)
 		tab[-1]['Pair_ID'] = i
 		tab[-1]['ra'] = ra[i]
 		tab[-1]['dec'] = dec[i]
-		tab[-1]['t'] = a[0]['t'] - 2400000.5 # all lines in a given nightly file have the same time stamp.
-		tab[-1][b] = a[ind_near[ind]]['P' + b + '_cal']
-		tab[-1][b + '_err'] = a[ind_near[ind]]['P' + b + '_cal_err']
+		tab[-1]['t'] = datadict['data' + b]['t'][i1] - 2400000.5 # all lines in a given nightly file have the same time stamp.
+		tab[-1][b] = datadict['data' + b]['P' + b + '_cal'][i1]
+		tab[-1][b + '_err'] = datadict['data' + b]['P' + b + '_cal_err_corr'][i1]
 	    
-    asciitable.write(tab, input_info.resultfolder + b + '_pairitel.csv' )
+	asciitable.write(tab, input_info.resultfolder + b + '_pairitel.csv' )
 
+# if pairitel_astropy_5.oy has been run before, use this:
+# (it collects the calibrated magnitudes of each night into light curves for each source)
+make_lcs()
 
-
+# if something has chrashed and the calibrated data needs to be read from hard drive, run this:
+# (it does the same, but takes longer)
+#make_lcs_from_disk()
